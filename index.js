290154e8ce8770b4038234d8ab89615abfa0660d
Priyanka Danda <priyankadanda47@gmail.com>
2018-06-19 15:48:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // write your code below!
 function happyHolidays()
 {
-  return "Happy Holidays!"
+  return "Happy holidays!"
 }
 
 function happyHolidaysTo(string)
 {
-  return `Happy Holidays, ${string}!`
+  return `Happy holidays, ${string}!`
 }
 function happyHolidayTo(holiday,name)
 {
